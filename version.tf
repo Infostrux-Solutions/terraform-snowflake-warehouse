0cde7a259545d29b738777522e1b28281db6e2c6
Charles-Etienne Couture <charles@infostrux.com>
2022-05-30 10:55:41 -0400

    fix(Snowflake-source-path): The provider have change the source path

--- version.tf
+++ version.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.13.1"
   required_providers {
     snowflake = {
-      source  = "chanzuckerberg/snowflake"
+      source  = "Snowflake-Labs/snowflake"
       version = ">=0.31.0"
     }
   }
